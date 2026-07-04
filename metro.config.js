65adc871bc48847c3eb07c7253715ed7ddaebd0e
dimash <abdykendinmuhammed@gmail.com>
2026-07-05 02:28:50 +0500

    chore(icons): set up react-native-svg-transformer + add quick-log SVG assets

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,11 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// SVG-as-component imports (react-native-svg-transformer): route .svg through the
+// SVG babel transformer and move "svg" out of asset extensions into source
+// extensions. Merged into the default config before withNativeWind wraps it.
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
 module.exports = withNativeWind(config, { input: './src/global.css' });
